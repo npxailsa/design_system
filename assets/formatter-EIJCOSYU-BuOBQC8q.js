1cbd05d1b4c604a72754a3943cf8d5774b1f1925
npxailsa <ailsa.blair@npxinnovation.ca>
2026-03-25 17:11:54 +0000

    Deploying to gh-pages from @ npxailsa/design_system@cc097720cd9d15ac7b79def3fc006bef3d0683ff 🚀

--- assets/formatter-EIJCOSYU-BuOBQC8q.js
+++
@@ -1,1 +0,0 @@
-import{f as m,r as e}from"./blocks-CJW7ldyC.js";import{b as o}from"./iframe-BBNjhiEI.js";import"./preload-helper-Dp1pzeXC.js";import"./index-CHkT-oC9.js";var t=o(e(),1),_=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
